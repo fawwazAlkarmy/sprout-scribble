ef64c38daebbb2ba269b1ae0fafcbf5b118f70fd
fawwazAlkarmy <alkarmyfawwaz@gmail.com>
2025-04-19 16:57:34 +0300

    Separate submit button in a client component to use useFormStatus

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import PostButton from "@/components/post-button";
 import createPost from "@/server/actions/create-post";
 import getPosts from "@/server/actions/get-posts";
 
@@ -5,14 +6,24 @@ export default async function Home() {
   const posts = await getPosts();
 
   return (
-    <div>
-      <h1>Home</h1>
-      {posts.success &&
-        posts.success.map((post) => <p key={post.id}>{post.title}</p>)}
-
-      <form action={createPost}>
-        <input type="text" name="title" />
-        <button type="submit">Submit</button>
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-bold mb-4">Blog Posts</h1>
+      <ul className="w-full">
+        {posts.success &&
+          posts.success.map((post) => (
+            <li key={post.id} className="mb-4">
+              <p className="text-xl">{post.title}</p>
+            </li>
+          ))}
+      </ul>
+      <form action={createPost} className="flex flex-col items-center w-full">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="bg-white border-2 border-gray-300 px-3 py-2 rounded-md mb-4"
+        />
+        <PostButton />
       </form>
     </div>
   );
